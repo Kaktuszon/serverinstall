be2991f32650d83f47111caa20024acd24b0836d
Kaktuszon <kaktus@kaktuszon.org>
2023-10-08 21:56:09 +0200

    The installation script. Download this and run on servers

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt install -y wget curl vim
+curl https://raw.githubusercontent.com/Kaktuszon/serverinstall/main/.bashrc > ~/.bashrc
+curl https://raw.githubusercontent.com/Kaktuszon/serverinstall/main/.vimrc > ~/.vimrc
