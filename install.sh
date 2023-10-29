ceeacb6030f4808b7e5145c766e750991620254a
Kaktuszon <kaktus@kaktuszon.org>
2023-10-29 16:17:17 +0100

    Added htop and screen

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-sudo apt install -y wget curl vim
+sudo apt install -y wget curl vim htop screen
 curl https://raw.githubusercontent.com/Kaktuszon/serverinstall/main/.bashrc > ~/.bashrc
 curl https://raw.githubusercontent.com/Kaktuszon/serverinstall/main/.vimrc > ~/.vimrc
